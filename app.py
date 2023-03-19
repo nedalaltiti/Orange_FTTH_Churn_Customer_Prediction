1b0484ada24ada05b918f18c00f4d90046ed10dd
Nedal <nedal.altiti@live.com>
2023-03-20 01:39:15 +0300

    Changed Return Value

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    eva = ""
     test_file = pd.read_csv("test.csv")
     test = orange_project.preprocessing(test_file)
     X_test, y_test = test.drop('TARGET', axis=1), test['TARGET']
@@ -16,8 +15,7 @@ def hello():
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
         y_pred = model.predict(X_test)
-        eva = orange_project.evaluation(y_true=y_test,y_pred=y_pred)
-        return eva
+        return orange_project.evaluation(y_true=y_test,y_pred=y_pred)
 
 
 if __name__ == "__main__":
